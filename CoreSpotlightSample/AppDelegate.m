53841d84b17febe2edca5445450bac002a429d8e
Oisin Prendiville <oisin@supertop.co>
2015-10-23 16:39:05 +0100

    Adds placeholder podcast/episode indexing

--- CoreSpotlightSample/AppDelegate.m
+++ CoreSpotlightSample/AppDelegate.m
@@ -6,9 +6,13 @@
 //  Copyright © 2015 Oisin Prendiville. All rights reserved.
 //
 
+@import CoreSpotlight;
+#import <MobileCoreServices/MobileCoreServices.h>
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
+
+@property (nonatomic, strong) NSDateFormatter *dateFormatter;
 
 @end
 
@@ -17,6 +21,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.dateFormatter = [[NSDateFormatter alloc] init];
+    self.dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+    
+    [self resetIndex];
     return YES;
 }
 
@@ -40,6 +49,83 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+- (void)resetIndex
+{
+    [[CSSearchableIndex defaultSearchableIndex] deleteAllSearchableItemsWithCompletionHandler:^(NSError * _Nullable error) {
+        if (!error) {
+            NSLog(@"Removed everything from index");
+            
+            [self createIndex];
+            
+        } else {
+            NSLog(@"Failed to remove from index with error %@",error);
+        }
+    }];
+}
+
+- (void)createIndex
+{
+    [self addPodcastToIndex];
+}
+
+- (void)addPodcastToIndex
+{
+    CSSearchableItemAttributeSet *attributes = [[CSSearchableItemAttributeSet alloc] initWithItemContentType:(NSString *)kUTTypeImage];
+    attributes.title = @"Sample Podcast";
+    attributes.contentDescription = @"A Sample Podcast used to illustrate this CoreSpotlight issue";
+    attributes.thumbnailData = UIImagePNGRepresentation([UIImage imageNamed:@"artwork"]);
+    
+    NSString *identifier = @"co.supertop.podcast.sample";
+    
+    CSSearchableItem *item = [[CSSearchableItem alloc] initWithUniqueIdentifier:identifier
+                                                               domainIdentifier:@"co.supertop.podcast"
+                                                                   attributeSet:attributes];
+    
+    [[CSSearchableIndex defaultSearchableIndex] indexSearchableItems:@[item] completionHandler:^(NSError * _Nullable error) {
+        if (!error) {
+            NSLog(@"Added Sample Podcast to index");
+        } else {
+            NSLog(@"Failed to index Sample Podcast with error %@", error);
+        }
+    }];
+    
+    for (NSUInteger i = 1; i < 100; i++) {
+        [self addEpisodeToIndexWithEpisodeNumber:i];
+    }
+}
+
+- (void)addEpisodeToIndexWithEpisodeNumber:(NSUInteger)episodeNumber
+{
+    NSString *episodeTitle = [NSString stringWithFormat:@"Sample Podcast: Episode %lu", episodeNumber];
+    NSDate *episodeReleaseDate = [NSDate dateWithTimeIntervalSinceReferenceDate:(episodeNumber * 604800)];
+    
+    CSSearchableItemAttributeSet *attributes = [[CSSearchableItemAttributeSet alloc] initWithItemContentType:(NSString *)kUTTypeAudio];
+    attributes.title = episodeTitle;
+    attributes.contentDescription = [NSString stringWithFormat:@"[%@] Episode %lu of this fictional sample podcast", [self.dateFormatter stringFromDate:episodeReleaseDate], episodeNumber];
+    attributes.thumbnailData = UIImagePNGRepresentation([UIImage imageNamed:@"artwork"]);
+    attributes.metadataModificationDate = episodeReleaseDate;
+    
+    attributes.containerTitle = @"Sample Podcast";
+    attributes.containerIdentifier = @"co.supertop.podcast.sample";
+    attributes.containerOrder = @(episodeNumber);
+    
+    NSString *identifier = [NSString stringWithFormat:@"co.supertop.podcast.sample.episode.%lu",episodeNumber];
+    NSString *domain = [NSString stringWithFormat:@"co.supertop.episode.podcast.sample.%lu",episodeNumber];
+    
+    CSSearchableItem *item = [[CSSearchableItem alloc] initWithUniqueIdentifier:identifier
+                                                               domainIdentifier:domain
+                                                                   attributeSet:attributes];
+    
+    [[CSSearchableIndex defaultSearchableIndex] indexSearchableItems:@[item] completionHandler:^(NSError * _Nullable error) {
+        if (!error) {
+            NSLog(@"Added %@ to index", episodeTitle);
+        } else {
+            NSLog(@"Failed to index %@ with error %@", episodeTitle, error);
+        }
+    }];
 }
 
 @end
